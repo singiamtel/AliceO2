f0f5af049b1a22483351abc644d608fdae30e0b2
Sergio <sergio.garcia.de.la.cruz@cern.ch>
2025-01-17 10:39:07 +0100

    bad

--- run/o2sim_evalmat.cxx
+++ run/o2sim_evalmat.cxx
@@ -17,35 +17,35 @@
 
 int main(int argc, char* argv[])
 {
-  TStopwatch timer;
-  timer.Start();
+	TStopwatch timer;
+	timer.Start();
 
-  auto& conf = o2::conf::SimConfig::Instance();
+	auto& conf = o2::conf::SimConfig::Instance();
 #ifdef SIM_RUN5
-  conf.setRun5();
+	conf.setRun5();
 #endif
-  auto& matmapP = o2::conf::MatMapParams::Instance();
-  if (!conf.resetFromArguments(argc, argv)) {
-    return 1;
-  }
-  // custom number of events
-  // customize the level of output
-  FairLogger::GetLogger()->SetLogScreenLevel(conf.getLogSeverity().c_str());
-  FairLogger::GetLogger()->SetLogVerbosityLevel(conf.getLogVerbosity().c_str());
+	auto& matmapP = o2::conf::MatMapParams::Instance();
+	if (!conf.resetFromArguments(argc, argv)) {
+		return 1;
+	}
+	// custom number of events
+	// customize the level of output
+	FairLogger::GetLogger()->SetLogScreenLevel(conf.getLogSeverity().c_str());
+	FairLogger::GetLogger()->SetLogVerbosityLevel(conf.getLogVerbosity().c_str());
 
-  // call o2sim "macro"
-  o2sim(false, true);
+	// call o2sim "macro"
+	o2sim(false, true);
 
-  o2::utils::ShmManager::Instance().release();
+	o2::utils::ShmManager::Instance().release();
 
-  // print total time
-  LOG(info) << "Simulation process took " << timer.RealTime() << " s";
+	// print total time
+	LOG(info) << "Simulation process took " << timer.RealTime() << " s";
 
-  // We do this instead of return 0
-  // for the reason that we see lots of problems
-  // with TROOTs atexit mechanism often triggering double-free or delete symptoms.
-  // While this is not optimal ... I think it is for the moment
-  // better to have a stable simulation runtime in contrast to
-  // having to debug complicated "atexit" memory problems.
-  _exit(0);
+	// We do this instead of return 0
+	// for the reason that we see lots of problems
+	// with TROOTs atexit mechanism often triggering double-free or delete symptoms.
+	// While this is not optimal ... I think it is for the moment
+	// better to have a stable simulation runtime in contrast to
+	// having to debug complicated "atexit" memory problems.
+	_exit(0);
 }
